d7ac7bfec43b5fa65515935b42cde03843fe516b
AndresPortoCastro <101732746+leviescan@users.noreply.github.com>
2022-09-28 16:56:05 -0500

    seconj

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,5 @@
+import { FormatPainterFill } from './../../../node_modules/@ant-design/icons-angular/src/icons/fill/FormatPainterFill';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,10 +10,34 @@ import { Component, OnInit } from '@angular/core';
 export class LoginComponent implements OnInit {
 
   hide:boolean=true;
+  public form!: FormGroup;
 
-  constructor() { }
+  constructor( private fb:FormBuilder) {
+    this.form = this.fb.group({
+      email: ["",[Validators.required,Validators.email]],
+      password:["",[Validators.required, Validators.minLength(7)]],
+      show:["",[Validators.required, Validators.requiredTrue]]
+
+    });
+  }
 
   ngOnInit(): void {
+
+    this.loadApi();
+  }
+
+  loadApi():any{
+    const respon = {
+      email:'andres@hmail.com',
+      password:'1234567',
+      show: true
+    };
+
+    this.form.patchValue({
+      email:respon.email,
+      password:respon.password,
+      show:respon.show,
+    })
   }
 
 }
